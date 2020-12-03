39aa54d5a643ed83c8ed6cf6808d977d5cfbf09f
unknown <madhusudannayak80@gmail.com>
2020-12-03 12:14:24 +0530

    first commit

---
+++ app/src/main/java/com/example/listviewexample/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.listviewexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView=findViewById(R.id.list);
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(0);
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+        arrayList.add(4);
+        arrayList.add(5);
+        arrayList.add(6);
+        arrayList.add(7);
+        arrayList.add(8);
+        arrayList.add(9);
+        arrayList.add(10);
+        arrayList.add(11);
+        arrayList.add(12);
+        arrayList.add(13);
+        arrayList.add(14);
+        arrayList.add(15);
+        arrayList.add(16);
+        arrayList.add(17);
+        arrayList.add(18);
+        arrayList.add(19);
+
+        ArrayAdapter<Integer> arrayAdapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_list_item_1,arrayList);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this,""+position,Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+
+
+    }
+}
